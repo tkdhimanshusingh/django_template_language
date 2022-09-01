d2e6309a27fb2e6d9836328327c45163d6bfe36a
Himanshu Singh <tkdhimanshusingh@gmail.com>
2022-09-01 22:09:24 +0530

    Dynamic variable used

--- course/views.py
+++ course/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def learn_django(request):
-    return render(request,'course/courseone.html')
+    return render(request,'course/courseone.html',{'nm':'Django'})
 def learn_python(request):
-    return render(request,'course/coursetwo.html')
+    return render(request,'course/courseone.html',{'nm':'Python'})
