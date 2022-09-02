42e0698d73a18eac1d6d94ba152121e5b8bd55c1
Himanshu Singh <tkdhimanshusingh@gmail.com>
2022-09-02 21:03:38 +0530

    capfirst filter illustration

--- course/views.py
+++ course/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def learn_django(request):
-    cname='Django'
+    cname='django'
     duration='6 Months'
     seats=10
     django_details={'nm':cname,'du':duration,'st':seats}
     return render(request,'course/courseone.html',django_details)
 def learn_python(request):
-    cname='Python'
+    cname='python'
     duration='3 Months'
     seats=60
     django_details={'nm':cname,'du':duration,'st':seats}
