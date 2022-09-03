24981ba0d6664b1bb4d587d9cc486330c20dce87
Himanshu Singh <tkdhimanshusingh@gmail.com>
2022-09-03 08:49:25 +0530

    using the 'lower' filter

--- course/views.py
+++ course/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def learn_django(request):
-    cname='django'
+    cname='DJANGO'
     duration='6 Months'
     seats=10
     django_details={'nm':cname,'du':duration,'st':seats}
     return render(request,'course/courseone.html',django_details)
 def learn_python(request):
-    cname='python'
+    cname='PYTHON'
     duration='3 Months'
     seats=60
     django_details={'nm':cname,'du':False,'st':seats}
